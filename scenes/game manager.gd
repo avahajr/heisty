0e42216501b2b4c2ddd35b38bd9f7f4f21638299
marriedtoafrog <michellezhou1024@gmail.com>
2024-05-05 19:05:29 -0700

    corrected name

--- scenes/game manager.gd
+++ scenes/game manager.gd
@@ -8,23 +8,23 @@ func addPink():
 		points = points + 2 
 		print(points)
 		pointLabel.text = "Total Points: " + str(points)
-	check_end_scence()
+	check_end_scene()
 
 func addBlue(): 
 	if(points != 10):
 		points = points + 1
 		print(points)
 		pointLabel.text = "Total Points: " + str(points) 
-	check_end_scence()
+	check_end_scene()
 
 func addChest(): 
 	if(points != 10):
 		points = points + 5
 		print(points)
 		pointLabel.text = "Total Points: " + str(points)
-	check_end_scence()
+	check_end_scene()
 
-func check_end_scence(): 
+func check_end_scene(): 
 	if(points == 10): 
 		get_tree().change_scene_to_file("res://scenes/win scene.tscn")
 	
